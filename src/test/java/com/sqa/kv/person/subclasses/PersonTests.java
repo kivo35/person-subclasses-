1a386f4d136be3f5d1afe0b9a0ff5df4fc4214de
Kirill <kivo35@hotmail.com>
2016-02-04 11:19:12 -0800

    Create test methods for Person and Employee classes

---
+++ src/test/java/com/sqa/kv/person/subclasses/PersonTests.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.sqa.kv.person.subclasses;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Kirill
+ * 
+ */
+public class PersonTests
+{
+
+	@Test
+	public void testEmployeeActions()
+	{
+		Person[] people = new Person[] { new Employee(), new Person(), new Employee("KV", 22, 5.0, 45, "SQA School"),
+				new Person("KV", 22, 5.0), new Employee("Jack", 12, 0.0, 33, "Random company"),
+				new Person("Jack", 12, 0.0) };
+		for (Person p : people)
+		{
+			// Do Work
+			p.doWork();
+			// Determine Stress
+			System.out.println("\tStress level: " + p.calcStressLevel());
+			// Determine Pay
+			System.out.println("\tPay amount: " + p.calcPaid());
+			// call toString to get all details
+			System.out.println(p.toString());
+		}
+	}
+
+	@Ignore
+	public void testPersonActions()
+	{
+		Person[] people = new Person[] { new Person(), new Person("KV", 22, 5.0), new Person("Jack", 12, 0.0) };
+
+		for (Person p : people)
+		{
+			// Do Work
+			p.doWork();
+			// Determine Stress
+			System.out.println("\tStress level: " + p.calcStressLevel());
+			// Determine Pay
+			System.out.println("\tPay amount: " + p.calcPaid());
+			// call toString to get all details
+			System.out.println(p.toString());
+		}
+	}
+}
