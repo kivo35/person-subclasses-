ef92e23af3bad1fb6f58b132e9413ab25a6d1d44
Kirill <kivo35@hotmail.com>
2016-02-04 19:04:57 -0800

    Add Teacher class test method

--- src/test/java/com/sqa/kv/person/subclasses/PersonTests.java
+++ src/test/java/com/sqa/kv/person/subclasses/PersonTests.java
@@ -12,8 +12,7 @@ import org.junit.Test;
  */
 public class PersonTests
 {
-
-	@Test
+	@Ignore
 	public void testEmployeeActions()
 	{
 		Person[] people = new Person[] { new Employee(), new Person(), new Employee("KV", 22, 5.0, 45, "SQA School"),
@@ -37,6 +36,26 @@ public class PersonTests
 	{
 		Person[] people = new Person[] { new Person(), new Person("KV", 22, 5.0), new Person("Jack", 12, 0.0) };
 
+		for (Person p : people)
+		{
+			// Do Work
+			p.doWork();
+			// Determine Stress
+			System.out.println("\tStress level: " + p.calcStressLevel());
+			// Determine Pay
+			System.out.println("\tPay amount: " + p.calcPaid());
+			// call toString to get all details
+			System.out.println(p.toString());
+		}
+	}
+
+	@Test
+	public void testTeacherActions()
+	{
+		Person[] people = new Person[] { new Person(), new Employee(), new Teacher(),
+				new Employee("KV", 22, 5.0, 45, "SQA School"), new Person("KV", 22, 5.0),
+				new Teacher("Jack", 45, 1000.23, 1235223, "Public school", "Math Teacher"),
+				new Employee("Jack", 12, 0.0, 33, "Random company"), new Person("Jack", 12, 0.0) };
 		for (Person p : people)
 		{
 			// Do Work
